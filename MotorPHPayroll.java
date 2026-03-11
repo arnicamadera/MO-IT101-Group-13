dee5107bf5f61636ae37ec91107cc6e3ddf66ea7
arnicamadera <lr.amadera@mmdc.mcl.edu.ph>
2026-03-11 14:10:33 +0800

    Update MotorPHPayroll.java

--- MotorPHPayroll.java
+++ MotorPHPayroll.java
@@ -294,54 +294,98 @@ public class MotorPHPayroll {
         System.out.println("===================================================");
     }
 
-    static double computeEmployeeSSS(double salary){
-        if (salary<4250) return 180.00; else if(salary<4750) return 202.5;
-        else if(salary<5250) return 225.00; else if(salary<5750) return 247.50;
-        else if(salary<6250) return 270.00; else if(salary<6750) return 292.50;
-        else if(salary<7250) return 315.00; else if(salary<7750) return 337.50;
-        else if(salary<8250) return 360.00; else if(salary<8750) return 382.50;
-        else if(salary<9250) return 405.00; else if(salary<9750) return 427.50;
-        else if(salary<10250) return 450.00; else if(salary<10750) return 472.50;
-        else if(salary<11250) return 495.00; else if(salary<11750) return 517.50;
-        else if(salary<12250) return 540.00; else if(salary<12750) return 562.50;
-        else if(salary<13250) return 585.00; else if(salary<13750) return 607.50;
-        else if(salary<14250) return 630.00; else if(salary<14750) return 652.50;
-        else if(salary<15250) return 675.00; else if(salary<15750) return 697.50;
-        else if(salary<16250) return 720.00; else if(salary<16750) return 742.50;
-        else if(salary<17250) return 765.00; else if(salary<17750) return 787.50;
-        else if(salary<18250) return 810.00; else if(salary<18750) return 832.50;
-        else if(salary<19250) return 855.00; else if(salary<19750) return 877.50;
-        else if(salary<20250) return 900.00; else if(salary<20750) return 922.50;
-        else if(salary<21250) return 945.00; else if(salary<21750) return 967.50;
-        else if(salary<22250) return 990.00; else if(salary<22750) return 1012.50;
-        else if(salary<23250) return 1035.00; else if(salary<23750) return 1057.50;
-        else if(salary<24250) return 1080.00; else if(salary<24750) return 1102.50;
-        else if(salary<25250) return 1125.00; else if(salary<25750) return 1147.50;
-        else if(salary<26250) return 1170.00; else if(salary<26750) return 1192.50;
-        else if(salary<27250) return 1215.00; else if(salary<27750) return 1237.50;
-        else if(salary<28250) return 1260.00; else if(salary<28750) return 1282.50;
-        else if(salary<29250) return 1305.00; else if(salary<29750) return 1327.50;
-        else return 1350.00;
+
+    //  ---------- CONTRIBUTIONS & DEDUCTIONS ----------
+    
+    // ---------- COMPUTE SSS CONTRIBUTION ----------
+    // based on MotorPH's SSS table
+    
+    static double computeEmployeeSSS(double salary) {
+        if (salary < 3250) return 135.00;
+        else if (salary < 3750) return 157.50;
+        else if (salary < 4250) return 180.00;
+        else if (salary < 4750) return 202.50;
+        else if (salary < 5250) return 225.00;
+        else if (salary < 5750) return 247.50;
+        else if (salary < 6250) return 270.00;
+        else if (salary < 6750) return 292.50;
+        else if (salary < 7250) return 315.00;
+        else if (salary < 7750) return 337.50;
+        else if (salary < 8250) return 360.00;
+        else if (salary < 8750) return 382.50;
+        else if (salary < 9250) return 405.00;
+        else if (salary < 9750) return 427.50;
+        else if (salary < 10250) return 450.00;
+        else if (salary < 10750) return 472.50;
+        else if (salary < 11250) return 495.00;
+        else if (salary < 11750) return 517.50;
+        else if (salary < 12250) return 540.00;
+        else if (salary < 12750) return 562.50;
+        else if (salary < 13250) return 585.00;
+        else if (salary < 13750) return 607.50;
+        else if (salary < 14250) return 630.00;
+        else if (salary < 14750) return 652.50;
+        else if (salary < 15250) return 675.00;
+        else if (salary < 15750) return 697.50;
+        else if (salary < 16250) return 720.00;
+        else if (salary < 16750) return 742.50;
+        else if (salary < 17250) return 765.00;
+        else if (salary < 17750) return 787.50;
+        else if (salary < 18250) return 810.00;
+        else if (salary < 18750) return 832.50;
+        else if (salary < 19250) return 855.00;
+        else if (salary < 19750) return 877.50;
+        else if (salary < 20250) return 900.00;
+        else if (salary < 20750) return 922.50;
+        else if (salary < 21250) return 945.00;
+        else if (salary < 21750) return 967.50;
+        else if (salary < 22250) return 990.00;
+        else if (salary < 22750) return 1012.50;
+        else if (salary < 23250) return 1035.00;
+        else if (salary < 23750) return 1057.50;
+        else if (salary < 24250) return 1080.00;
+        else if (salary < 24750) return 1102.50;
+        else return 1125.00;
     }
 
+
+    // ---------- COMPUTE PHILHEALTH CONTRIBUTION ----------
+    // based on MotorPH's PhilHealth table
+    
     static double computePhilHealth(double salary){
-        if(salary <= 10000) return 10000 * 0.025;
-        else if(salary < 100000) return salary * 0.025;
-        else return 2500;
+        if(salary <= 60000){
+            return salary * 0.015; // 50% of 3%
+        } else {
+            return 900; // 50% of maximum 1800
+        }
     }
+
+    // ---------- COMPUTE PAGIBIG CONTRIBUTION ----------
+    // based on MotorPH's PAGIBIG table
 
     static double computePagibig(double salary){
-        if (salary <= 1500) return salary * 0.01;
-        else if (salary <= 10000) return salary * 0.02;
-        else return 200;
+        double pagibig;
+        if (salary <= 1500){
+            pagibig = salary * 0.01; // employee's contribution only
+        }
+        else {
+            pagibig = salary * 0.02;
+        }
+        if (pagibig > 100) pagibig = 100; // maximum contribution amount
+
+        return pagibig;
     }
 
-    static double computeTrainTax(double taxable){
-        if(taxable<=20833) return 0;
-        else if(taxable<=33333) return (taxable-20833)*0.15;
-        else if(taxable<=66667) return 1875 + (taxable-33333)*0.20;
-        else if(taxable<=166667) return 8541 + (taxable-66667)*0.25;
-        else if(taxable<=666667) return 33541.80 + (taxable-166667)*0.30;
-        else return 183541.80 + (taxable-666667)*0.35;
+    // ---------- COMPUTE TAX DEDUCTION ----------
+    // based on MotorPH's Tax table
+    
+    static double computeTrainTax(double taxable) {
+        if(taxable <= 20832) return 0;
+        else if(taxable <= 33332) return (taxable - 20832) * 0.20;
+        else if(taxable <= 66666) return 2500 + (taxable - 33333) * 0.25;
+        else if(taxable <= 166666) return 10833 + (taxable - 66667) * 0.30;
+        else if(taxable <= 666666) return 40833.33 + (taxable - 166667) * 0.32;
+        else return 200833.33 + (taxable - 666667) * 0.35;
     }
 }
+
